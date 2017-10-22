b7461762c4d02c3068af05d3026a95ed881826af
Fabian Fürst <fa.fuerst@gmail.com>
2017-10-22 14:17:31 +0200

    add Search functionality

--- src/Search.js
+++ src/Search.js
@@ -1,27 +1,49 @@
 import React, { Component } from 'react'
+import * as BooksAPI from './BooksAPI'
 import { Link } from 'react-router-dom'
+import Book from './Book'
 
 class SearchBooks extends Component {
+  state = {
+    query: '',
+    foundBooks: []
+  }
+
+  searchBooks = (event) => {
+    const query = event.target.value.trim()
+    this.setState({query: query})
+
+    if (query) {
+      BooksAPI.search(query, 20).then((books) => {
+        this.setState({foundBooks: books})
+      })
+    }
+
+  }
+
   render() {
+    const { books, changeShelf } = this.props
+    const { query, foundBooks } = this.state
+
     return (
       <div className="search-books">
         <div className="search-books-bar">
           <Link className="close-search" to='/'>Close</Link>
           <div className="search-books-input-wrapper">
-            {/*
-              NOTES: The search from BooksAPI is limited to a particular set of search terms.
-              You can find these search terms here:
-              https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-              However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-              you don't find a specific author or title. Every search is limited by search terms.
-            */}
-            <input type="text" placeholder="Search by title or author"/>
-
+            <input type="text" placeholder="Search by title or author" value={query} onChange={this.searchBooks}/>
           </div>
         </div>
         <div className="search-books-results">
-          <ol className="books-grid"></ol>
+          <ol className="books-grid">
+            {foundBooks.map((book) =>
+              <div className="bookshelf-books">
+                <Book
+                books={books}
+                book={book}
+                changeShelf={ changeShelf }/>
+              </div>
+            )}
+          </ol>
         </div>
       </div>
     )
